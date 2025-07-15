70e8346c9caea508dd2480635b403e35780fabab
Sreedev Rajendran <sreerajar40@gmail.com>
2025-07-15 20:53:47 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,41 @@
+public class BankAccount {
+    private String accountHolder;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolder, String accountNumber) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Invalid or insufficient funds.");
+        }
+    }
+}
